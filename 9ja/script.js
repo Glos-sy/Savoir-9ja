61c5bfd0f5ad2e9a7ce272259767eecd8ad11d8d
Ayo <baniremubarakah@gmail.com>
2025-03-16 17:48:23 +0100

    nav bar adjustments

--- 9ja/script.js
+++ 9ja/script.js
@@ -1,38 +1,8 @@
 "use strict";
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function autoSlide() {
-  nextSlide(1);
-}
-
-setInterval(autoSlide, 3000);
-
-function nextSlide(n) {
-  showSlides((slideIndex = slideIndex + n));
-}
-
-function showSlides(n) {
-  const slides = document.getElementsByClassName("festivals-slide");
-
-  if (n > slides.length) {
-    slideIndex = 1;
-  } else if (n < 1) {
-    slideIndex = slides.length;
-  } else {
-    slideIndex = n;
-  }
-
-  Array.from(slides).forEach((slide) => {
-    slide.style.display = "none";
-  });
-
-  // UPDATED STYLING
-  slides[slideIndex - 1].style.display = "block";
-}
-
-function screenSizeChange(mediaQuery) {
-  if (mediaQuery.matches) {
-  }
-}
+document.querySelector(".fa-bars").addEventListener("click", function () {
+  const links = document.querySelector("#nav-bar");
+  const icon = document.querySelector("#bars");
+  links.classList.toggle("show");
+  icon.classList.toggle("fa-xmark");
+});
